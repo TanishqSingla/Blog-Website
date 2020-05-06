9b0c868fa06e7f08fe06b8a91b60019bd28d3c28
TanishqSingla <tanishqsingla00@gmail.com>
2020-05-06 23:43:40 +0530

    Getting user input in compose

--- app.js
+++ app.js
@@ -34,6 +34,10 @@ app.get("/compose", (req, res) => {
   res.render("compose");
 });
 
+app.post("/compose", (req, res) => {
+  console.log(req.body.publishedContent);
+});
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
